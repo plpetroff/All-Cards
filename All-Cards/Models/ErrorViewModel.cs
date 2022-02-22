e2674955aed2540e1ac43691b1bb57b2cb6e81d5
Pl. Petroff <plpetroff@gmail.com>
2022-02-22 14:01:07 +0200

    Add project files.

---
+++ All-Cards/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace All_Cards.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
